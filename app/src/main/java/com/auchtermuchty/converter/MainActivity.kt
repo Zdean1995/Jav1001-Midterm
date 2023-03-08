8ad702bd29999445b00f69a836db321f9610c9f6
Zachary <zdean1995@gmail.com>
2023-03-08 02:08:52 -0500

    Removed the button and added an automatically updating output value.  Added comments.

--- app/src/main/java/com/auchtermuchty/converter/MainActivity.kt
+++ app/src/main/java/com/auchtermuchty/converter/MainActivity.kt
@@ -1,8 +1,19 @@
 package com.auchtermuchty.converter
+/*
+Zachary Dean
+A00275392
+
+This is an android app that is used for converting between various distance units.  It is essentially an android version of
+the app I made for lab 1
+ */
+
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 import com.auchtermuchty.converter.databinding.ActivityMainBinding
 
 
@@ -14,28 +25,63 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.txtConverter.setOnClickListener {
-            val toast = Toast.makeText(this, "boner", Toast.LENGTH_SHORT)
-            toast.show()
+        //I managed to figure out this on my own.  I typed .listen at the end of txtDistanceInput and found this
+        //This makes it so the output text updates as the input text is changed
+        binding.txtDistanceInput.addTextChangedListener { setOutput() }
+
+        /*
+        The following is for making the output change as the unit spinners are changed.
+        onNothingSelected hasn't been touch since as far as I'm aware that's not possible
+        Taken from:
+        https://stackoverflow.com/questions/46447296/android-kotlin-onitemselectedlistener-for-spinner-not-working
+         */
+        binding.spnInputUnit.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                setOutput()
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+
+            }
+
         }
 
-        binding.btnConvert.setOnClickListener {
-
-            try {
-                val inputValue = binding.txtDistanceInput.text.toString().toDouble()
-                val inputDataType = getUnit(binding.spnInputUnit.selectedItem.toString())
-                val outputDataType = getUnit(binding.spnOutputUnit.selectedItem.toString())
-                val outputValue = inputValue * (inputDataType/outputDataType)
-                binding.txtDistanceOutput.setText(outputValue.toString())
-
-            } catch(exception: Exception) {
-                val toast = Toast.makeText(this, "Please enter an value", Toast.LENGTH_SHORT)
-                toast.show()
+        binding.spnOutputUnit.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                setOutput()
             }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+
+            }
+
+        }
+
+    }
+
+    /*
+    This function is for setting the output text.  It takes the input value and multiplies it by the
+    ratio of the two units.
+     */
+    fun setOutput() {
+        try {
+            val inputValue = binding.txtDistanceInput.text.toString().toDouble()
+            val inputDataType = getUnit(binding.spnInputUnit.selectedItem.toString())
+            val outputDataType = getUnit(binding.spnOutputUnit.selectedItem.toString())
+            val outputValue = inputValue * (inputDataType/outputDataType)
+            binding.txtDistanceOutput.setText(outputValue.toString())
+
+        } catch(exception: Exception) {
+            /*
+            This stops the app from crashing when the input value is empty.  It also stops the output
+            value from not changing when the input box is cleared
+             */
+            binding.txtDistanceOutput.setText("0")
         }
     }
 }
 
+//This is the same function used in my lab 1.
 fun getUnit (unit: String): Double {
     return when (unit){
         "mm" -> 1.0
